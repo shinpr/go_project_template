2c1cba8c9faf61a566204453017700feb4a3bd80
shinpr <shnskkgw.iueaaa@gmail.com>
2024-08-23 18:35:27 +0900

    add repository test

---
+++ go/internal/infrastructure/hello_test.go
@@ -0,0 +1,15 @@
+package infrastructure
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetMessage(t *testing.T) {
+	repository := NewHello()
+	m, err := repository.GetMessage()
+	require.NoError(t, err)
+	assert.Equal(t, m.Message, "Hello, World!")
+}
